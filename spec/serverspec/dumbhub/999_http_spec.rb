9727a794047adf40a563302d8d8360619313ba00
Tomoyuki Sakurai <y@trombik.org>
2020-01-05 18:48:19 +0900

    bugfix: add tests for the ntopng UI

---
+++ spec/serverspec/dumbhub/999_http_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require_relative "../spec_helper"
+require "net/http"
+
+host = ENV["TARGET_HOST"]
+ansible_host = inventory.host(host)["ansible_host"]
+port = 3000
+
+uri = "http://#{ansible_host}:#{port}"
+describe uri do
+  let(:http_get_response) do
+    Net::HTTP.get_response(URI(uri))
+  end
+
+  it "should return success" do
+    expect(http_get_response).to be_kind_of(Net::HTTPSuccess)
+     http_get_response.body
+  end
+
+  it "should include 'Welcome to ntopng'" do
+    expect(http_get_response.body).to match(/Welcome to ntopng/)
+  end
+
+  it "should show ntopng Community Edition is running" do
+    expect(http_get_response.body).to match(/ntopng Community Edition/)
+  end
+end
